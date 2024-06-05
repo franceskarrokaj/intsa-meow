fb2bd20349e9aebe9e90d2f68d92d903cd1a667a
franceska <franceska.rrokajh@epitech.eu>
2024-06-05 11:40:15 +0200

    [ADD] implement HomeScreen

---
+++ insta-meow/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text, SafeAreaView} from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>HOME SCREEN</Text>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default HomeScreen;
