fb8127b7b0a9faa156772194cafb804e11bb3b98
franceska <franceska.rrokajh@epitech.eu>
2024-06-08 21:49:30 +0200

    [MOD] postFooter

--- insta-meow/components/home/Post.js
+++ insta-meow/components/home/Post.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image, name} from 'react-native';
+import { StyleSheet, View, Text, Image, name, TouchableOpacity} from 'react-native';
 import { Divider } from 'react-native-elements';
 
 const postFooterIcons = [
     {
         name: 'Like',
-        imageUrl: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/'
+        imageUrl: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/like--v1.png',
+        likedImageUrl: "https://img.icons8.com/ios-glyphs/90/fa314a/like--v1.png"
+    },
+
+    {
+        name: 'Comment',
+        imageUrl: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/comments.png',     
+    },
+
+    {
+        name: 'Share',
+        imageUrl: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/share.png',       
+    },
+    
+    {
+        name: 'Save',
+        imageUrl: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/save.png',       
     },
 ]
 
@@ -15,6 +31,9 @@ const Post = ( {post} ) => {
             <Divider width={1} orientation='vertical' />
             <PostHeader post={post} />
             <PostImage post={post} />
+            <View style={{marginHorizontal: 15, marginTop: 10}}>
+                <PostFooter />
+            </View>
         </View>
     );
 }
@@ -55,14 +74,43 @@ const PostImage = ( {post} ) => (
     </View>
 )
 
+const PostFooter = () => (
+    <View style={{flexDirection: "row"}}>
+        <View style={styles.leftFooterIconContainer}>
+            <Icon imgStyle={styles.footerIcon} imageUrl={postFooterIcons[0].imageUrl}/>
+            <Icon imgStyle={styles.footerIcon} imageUrl={postFooterIcons[1].imageUrl}/>
+            <Icon imgStyle={styles.footerIcon} imageUrl={postFooterIcons[2].imageUrl}/>
+        </View>
+    </View>
+)
+
+const Icon = ({imgStyle, imageUrl}) => (
+    <TouchableOpacity>
+        <Image style={imgStyle} source={{ uri: imageUrl }} />
+    </TouchableOpacity>
+
+
+)
+
 const styles = StyleSheet.create({
     story: {
         width: 35,
         height: 35,
         borderRadius: 50,
-        marginLeft: 6,
+        marginLeft: 1.6,
         borderWidth: 1.6,
         borderColor: "#ff8501", 
+    },
+
+    footerIcon: {
+        width: 33,
+        height: 33,
+    },
+
+    leftFooterIconContainer: {
+        width: "32%",
+        flexDirection: "row",
+        justifyContent: "space-between",
     },
 })
 
