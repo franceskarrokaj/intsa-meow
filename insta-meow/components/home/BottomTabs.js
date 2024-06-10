1d6c6be3ac9599f055018e7f53de46d9a1132279
franceska <franceska.rrokajh@epitech.eu>
2024-06-10 12:05:48 +0200

    [MOD] bottom tabs

--- insta-meow/components/home/BottomTabs.js
+++ insta-meow/components/home/BottomTabs.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Image } from 'react-native';
 import {Divider} from 'react-native-elements';
 
-export const bottomTabs = [
+export const bottomTabsIcons = [
     {
         name: 'Home',
         active: 'https://img.icons8.com/fluency-systems-filled/144/ffffff/home.png',
@@ -30,9 +30,52 @@ export const bottomTabs = [
     },
 ]
 
-const BottomTabs = () => {
+const BottomTabs = ({icons}) => {
     const [activeTab, setActiveTab] = useState('Home');
 
+    const Icon = ({icon}) => (
+        <TouchableOpacity onPress={() => setActiveTab(icon.name)}>
+            <Image
+                source={{uri: activeTab === icon.name ? icon.active: icon.inactive}}
+                style={styles.icon}
+            />
+        </TouchableOpacity>
+    );
+
+    return (
+        <View style={styles.wrapper}>
+            <Divider width={1} orientation='vertical' />
+                <View style={styles.container}>
+                    {icons.map((icon, index) => (
+                        <Icon key={index} icon={icon} />
+                    ))}
+            </View>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+
+    wrapper: {
+        position: "absolute",
+        width: "100%",
+        bottom: "3%",
+        zIndex: 999,
+        backgroundColor: "#000",
+    },
+    
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        height: 45,
+        paddingTop: 10,
+
+    },
+
+    icon: {
+        width: 30,
+        height: 30,
+    },
+});
 
 export default BottomTabs;
