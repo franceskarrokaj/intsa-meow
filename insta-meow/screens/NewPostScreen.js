3ae8c107d65190b8ca868747324ca27fe845a635
franceska <franceska.rrokajh@epitech.eu>
2024-06-10 14:57:27 +0200

    [ADD] newpost screen

---
+++ insta-meow/screens/NewPostScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+
+const NewPostScreen = () => {
+    return (
+        <SafeAreaViewView style={{backgroundColor: "black", flex: 1}} >
+            <AddNewPost />
+        </SafeAreaViewView>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default NewPostScreen;
